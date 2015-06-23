131ad0f84f2bf545fb59f8380401a259aca8bf44
Bartosz Mąka <moncznybartosz@gmail.com>
2015-06-23 22:35:56 +0200

    Create Query Board.cpp

---
+++ easy-level/Query Board.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc,char*argv[])
+{
+    fstream file(argv[1],ios::in);
+    string buffer;
+    unsigned short int matrix[256][256];
+
+    for(unsigned short int i=0;i<256;i++)
+    {
+        for (unsigned short int j=0;j<256;j++)
+        {
+            matrix[i][j]=0;
+        }
+    }
+
+    while(!file.eof())
+    {
+        getline(file,buffer);
+        size_t pos=buffer.find(' ');
+
+        if (buffer[0]=='S')//Set
+        {
+            if(buffer[pos-1]=='w')//Row
+            {
+                buffer.erase(0,pos+1);
+                size_t pos=buffer.find(' ');
+                unsigned short int argi=atoi(buffer.substr(0,pos).c_str());
+                unsigned short int argx=atoi(buffer.substr(pos+1,buffer.length()-1).c_str());
+
+                for (unsigned short int i=0; i<256; i++)
+                {
+                    matrix[argi][i]=argx;
+                }
+//                cout<<"Matrix["<<argi<<"][*]= "<<argx<<endl;
+            }
+            else if (buffer[pos-1]=='l')//Col
+            {
+                buffer.erase(0,pos+1);
+                size_t pos=buffer.find(' ');
+                unsigned short int argi=atoi(buffer.substr(0,pos).c_str());
+                unsigned short int argx=atoi(buffer.substr(pos+1,buffer.length()-1).c_str());
+
+                for (unsigned short int i=0; i<256; i++)
+                {
+                    matrix[i][argi]=argx;
+                }
+//                cout<<"Matrix[*]["<<argi<<"]= "<<argx<<endl;
+            }
+        }
+        else if(buffer[0]=='Q')//Query
+        {
+            unsigned short int sum=0;
+            unsigned short int argi=atoi(buffer.substr(pos+1,buffer.length()-1).c_str());
+            if(buffer[pos-1]=='w')//Row
+            {
+                for (unsigned short int i=0; i<256; i++)
+                {
+                    sum+=matrix[argi][i];
+                }
+//                cout<<"sum of matrix ["<<argi<<"][*]= "<<sum<<endl;
+                cout<<sum<<endl;
+            }
+            else if (buffer[pos-1]=='l')//Col
+            {
+                for (int i=0; i<256; i++)
+                {
+                    sum+=matrix[i][argi];
+                }
+//                cout<<"sum of matrix [*]["<<argi<<"]= "<<sum<<endl;
+                cout<<sum<<endl;
+            }
+        }
+    }
+
+    file.close();
+    return 0;
+}
