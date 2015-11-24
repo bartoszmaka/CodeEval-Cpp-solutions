58517a51e44a0e377a3eb1177080edf20bcee17f
bartoszmaka <moncznybartosz@gmail.com>
2015-11-24 14:09:51 +0100

    updated medium/32

--- medium/32_trailing_string.py
+++ medium/32_trailing_string.py
@@ -3,7 +3,7 @@ import sys
 def foo(words):
     return 1 if words[1] in words[0] else 0
 
-file = open('32_testing_cases.txt')
+file = open(sys.argv[1])
 
 for line in file:
     line = list( line.strip().split(',') )
